1907e0c1edcc8978de2a5acd74135aced85b454c
Jared Frazier <cscidev001@gmail.com>
2025-05-27 17:07:17 +0200

    save image as 512x512 (necessary for omnisuite)

--- scripts/plot_random_temperature_on_latlon_grid.py
+++ scripts/plot_random_temperature_on_latlon_grid.py
@@ -14,6 +14,7 @@ n_frames = 366
 scale = 0.05  # spatial scale of noise
 time_scale = 0.02  # temporal scale of noise
 seed = 42
+dpi = 100
 
 # Function to generate fluid-like Perlin noise
 def generate_temperature_field(t, scale=0.05, time_scale=0.02):
@@ -29,7 +30,8 @@ def generate_temperature_field(t, scale=0.05, time_scale=0.02):
     return noise_field
 
 # Set up plot
-fig = plt.figure(figsize=(10, 5))
+# https://stackoverflow.com/questions/13714454/specifying-and-saving-a-figure-with-exact-size-in-pixels
+fig = plt.figure(figsize=(512/dpi, 512/dpi), dpi=dpi)
 ax = plt.axes(projection=ccrs.PlateCarree())
 ax.coastlines()
 temp = generate_temperature_field(0)
@@ -43,9 +45,9 @@ mesh = ax.pcolormesh(
 def update(frame):
     temp = generate_temperature_field(frame)
     mesh.set_array(temp)
-    #ax.set_title(f"Frame {frame}")
-    filename=f"data/frame_{frame}.png"
-    plt.savefig(filename, dpi=150, bbox_inches='tight')
+    filename=f"data/frame_{frame:03}.png"
+    # https://github.com/matplotlib/matplotlib/issues/2305#issuecomment-223996919
+    plt.savefig(filename, dpi='figure')
     return [mesh]
 
 # Animate
