097045903e07f90fa1b0f978703b195159054bdb
Jared Frazier <cscidev001@gmail.com>
2025-05-28 09:29:54 +0200

    provide basic pixel and animation flag to script

--- scripts/plot_random_temperature_on_latlon_grid.py
+++ scripts/plot_random_temperature_on_latlon_grid.py
@@ -1,8 +1,26 @@
+"""Plot perlin noise on arbitrary pixel Plate-Care projection. 
+
+usage: $0 x y animate
+
+where animate is {0,1}
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 from matplotlib.animation import FuncAnimation
 from noise import pnoise3  # Perlin noise in 3D
+import sys 
+
+
+if len(sys.argv) == 1:
+    x = 512
+    y = 512
+    animate = 1 
+else:
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    animate = int(sys.argv[3])
 
 # Grid definition
 lon = np.linspace(-180, 180, 360)
@@ -31,7 +49,7 @@ def generate_temperature_field(t, scale=0.05, time_scale=0.02):
 
 # Set up plot
 # https://stackoverflow.com/questions/13714454/specifying-and-saving-a-figure-with-exact-size-in-pixels
-fig = plt.figure(figsize=(512/dpi, 512/dpi), dpi=dpi)
+fig = plt.figure(figsize=(x/dpi, y/dpi), dpi=dpi)
 ax = plt.axes(projection=ccrs.PlateCarree())
 ax.coastlines()
 temp = generate_temperature_field(0)
@@ -51,6 +69,7 @@ def update(frame):
     return [mesh]
 
 # Animate
-ani = FuncAnimation(fig, update, frames=n_frames, interval=100)
+if animate:
+    ani = FuncAnimation(fig, update, frames=n_frames, interval=100)
 plt.show()
 
