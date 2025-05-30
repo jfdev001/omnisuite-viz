b699018a88d34911519c2867f8228fa656fe613e
Jared Frazier <frazier@iap-kborn.de>
2025-05-30 10:31:11 +0200

    WIP: better cli and interface...

--- scripts/plot_random_temperature_on_latlon_grid.py
+++ scripts/plot_random_temperature_on_latlon_grid.py
@@ -1,10 +1,6 @@
-"""Plot perlin noise on arbitrary pixel Plate-Care projection. 
+"""Plot perlin noise on arbitrary pixel Plate-Care projection."""
 
-usage: $0 x y animate
-
-where animate is {0,1}
-"""
-
+from argparse import ArgumentParser, BooleanOptionalAction
 import numpy as np
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
@@ -12,27 +8,55 @@ from matplotlib.animation import FuncAnimation
 from noise import pnoise3  # Perlin noise in 3D
 import sys 
 
+# define CLI 
+parser = ArgumentParser(
+    description="Plot (and save) animation of perlin noise on Plate-Care projection.")
 
-if len(sys.argv) == 1:
-    x = 512
-    y = 512
-    animate = 1 
-else:
-    x = int(sys.argv[1])
-    y = int(sys.argv[2])
-    animate = int(sys.argv[3])
+default_width = 4096
+parser.add_argument(
+    "width", 
+    type=int, 
+    help="width of image in pixels (default: {default_width})", 
+    default=default_width)
+
+default_height = 2048
+parser.add_argument(
+    "height", 
+    type=int, 
+    help=f"height of image in pixels (default: {default_height}", 
+    default=default_height)
+
+default_num_frames_in_animation = 366
+parser.add_argument(
+    "--num-frames-in-animation", 
+    help=f"default: {default_num_frames_in_animation}"
+    dest="num_frames_in_animation", 
+    type=int,
+    default=num_frames_in_animation)
+
+parser.add_argument("--animate", action=BooleanOptionalAction)
+
+default_dpi = 100
+parser.add_argument("--dpi", type=int, default=f"default: {default_dpi}")
+
+args = parser.parse_args()
+
+# Get cli args
+width : int = args.width 
+height : int = args.height  
+num_frames_in_animation : int = args.num_frames_in_animation 
+animate : bool = args.animate 
+dpi : int = args.dpi 
 
 # Grid definition
 lon = np.linspace(-180, 180, 360)
 lat = np.linspace(-90, 90, 180)
 lon2d, lat2d = np.meshgrid(lon, lat)
 
-# Parameters
-n_frames = 366
-scale = 0.05  # spatial scale of noise
-time_scale = 0.02  # temporal scale of noise
-seed = 42
-dpi = 100
+# Parameters for perlin noise 
+spatial_scale_of_perlin_noise = 0.05  
+temporal_scale_of_perlin_noise = 0.02  
+perlin_noise_seed = 42     
 
 # Function to generate fluid-like Perlin noise
 def generate_temperature_field(t, scale=0.05, time_scale=0.02):
@@ -40,21 +64,22 @@ def generate_temperature_field(t, scale=0.05, time_scale=0.02):
     for i in range(lat2d.shape[0]):
         for j in range(lat2d.shape[1]):
             noise_field[i, j] = pnoise3(
-                scale * lon2d[i, j],
-                scale * lat2d[i, j],
-                t * time_scale,
-                repeatx=360, repeaty=180, base=seed
+                spatial_scale_of_perlin_noise * lon2d[i, j],
+                spatial_scale_of_perlin_noise * lat2d[i, j],
+                t * temporal_scale_of_perlin_noise,
+                repeatx=360, repeaty=180, base=perlin_noise_seed
             )
     return noise_field
 
 # Set up plot
 # https://stackoverflow.com/questions/13714454/specifying-and-saving-a-figure-with-exact-size-in-pixels
-fig = plt.figure(figsize=(x/dpi, y/dpi), dpi=dpi)
+fig = plt.figure(figsize=(x/dpi, y/dpi))
 ax = plt.axes(projection=ccrs.PlateCarree())
 ax.coastlines()
 temp = generate_temperature_field(0)
 mesh = ax.pcolormesh(
-    lon, lat, 
+    lon, 
+    lat, 
     temp,
     transform=ccrs.PlateCarree(), 
     cmap='coolwarm')
@@ -65,11 +90,11 @@ def update(frame):
     mesh.set_array(temp)
     filename=f"data/frame_{frame:03}.png"
     # https://github.com/matplotlib/matplotlib/issues/2305#issuecomment-223996919
-    plt.savefig(filename, dpi='figure')
+    plt.savefig(filename, dpi=dpi)
     return [mesh]
 
 # Animate
 if animate:
-    ani = FuncAnimation(fig, update, frames=n_frames, interval=100)
+    ani = FuncAnimation(fig, update, frames=num_frames_in_animation, interval=100)
 plt.show()
 
