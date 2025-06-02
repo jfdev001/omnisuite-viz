09cc66cff8e373902d9e894cbc6d741c4ea4eae5
Jared Frazier <frazier@iap-kborn.de>
2025-06-02 12:01:05 +0200

    init tests, asserts in grid, and init abstract animator

--- omnisuite_examples/animator.py
+++ omnisuite_examples/animator.py
@@ -1,9 +1,28 @@
-class Animator:
-    def __init__(self):
+from abc import ABC, abstractmethod
+from omnisuite_examples.grid import Grid
+
+
+class Animator(ABC):
+    @abstractmethod
+    def __init__(self, grid: Grid):
         pass
 
+    @abstractmethod
     def animate(self):
         pass
 
-    def save(self):
+    @abstractmethod
+    def save_animation(self):
+        pass
+
+    @abstractmethod
+    def _save_frames(self):
+        pass
+
+    @abstractmethod
+    def _plot_initial_frame(self):
+        pass
+
+    @abstractmethod
+    def _update_frame(self):
         pass
