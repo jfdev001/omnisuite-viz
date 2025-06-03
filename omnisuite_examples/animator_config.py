d9d2a2cc3cf83bf7249436968485ce04f7e66ee2
Jared Frazier <frazier@iap-kborn.de>
2025-06-03 14:40:35 +0200

    animate passes test: animator, grid, config init refactor complete
    TODO:
    * Re-write main script in terms of these new classes
    * move explore tests into different dir and lib code into another?

--- omnisuite_examples/animator_config.py
+++ omnisuite_examples/animator_config.py
@@ -1,18 +1,48 @@
 from cartopy.crs import Projection, PlateCarree
 from dataclasses import dataclass
 from matplotlib.pyplot import rcParams
-from os.path import exists
-from typing import ClassVar, Tuple
+from os.path import exists, join
+from typing import ClassVar, Tuple, Dict
 
 
 @dataclass
 class AnimatorConfig:
-    INCH_PER_PIXEL: ClassVar[float] = 1 / rcParams['figure.dpi']
+
     save_animation: bool
+    num_frames_in_animation: int
+
     output_dir: str
+    path_to_save_animation: str = None
+
+    INCH_PER_PIXEL: ClassVar[float] = 1 / rcParams['figure.dpi']
+    formatted_file_name_per_frame: str = "%d.png"
 
     def __post_init__(self):
         assert exists(self.output_dir)
+
+        if self.path_to_save_animation is None:
+            self.path_to_save_animation = join(
+                self.output_dir, "animation.gif")
+
+        assert self._is_valid_file_name_format()
+
+    def _is_valid_file_name_format(self) -> bool:
+        return self._is_valid_percent_format()\
+            and self._is_valid_brace_format()
+
+    def _is_valid_brace_format(self) -> bool:
+        try:
+            self.formatted_file_name_per_frame.format(1)
+            return True
+        except (IndexError, KeyError, ValueError):
+            return False
+
+    def _is_valid_percent_format(self) -> bool:
+        try:
+            self.formatted_file_name_per_frame % 1
+            return True
+        except (TypeError, ValueError):
+            return False
 
 
 @dataclass
@@ -21,7 +51,10 @@ class OmniSuiteAnimatorConfig(AnimatorConfig):
     global_height_in_pixels: int = 2048
     figsize: Tuple[int, int] = (
         global_width_in_pixels*AnimatorConfig.INCH_PER_PIXEL,
-        global_width_in_pixels*AnimatorConfig.INCH_PER_PIXEL)
+        global_height_in_pixels*AnimatorConfig.INCH_PER_PIXEL)
+
+    pil_image_gif_loop: int = 0
+    pil_image_duration_between_frames_in_ms: float = 500
 
 
 @dataclass
--- tests/test_omnisuite_world_map_animator.py
+++ tests/test_omnisuite_world_map_animator.py
@@ -1,5 +1,7 @@
-import unittest
+import pdb
+from PIL import Image
 import tempfile
+import unittest
 
 from tests.animator_test_mixin import AnimatorTestMixin
 from omnisuite_examples.grid import WorldMapGrid
@@ -9,10 +11,19 @@ from omnisuite_examples.animator_config import OmniSuiteWorldMapAnimatorConfig
 
 class TestOmniSuiteWorldMapAnimator(AnimatorTestMixin, unittest.TestCase):
     def make_concrete_animator(self):
-        output_dir = ""  # TODO: use tempfile... elegant way to have context...
+
         grid = WorldMapGrid()
-        config = OmniSuiteWorldMapAnimatorConfig()
-        omnisuite_world_map_animator = OmniSuiteWorldMapAnimator(grid, config)
+
+        output_dir = self.temp_dir.name
+        self.num_frames_in_animation = 5
+        self._config = OmniSuiteWorldMapAnimatorConfig(
+            save_animation=True,
+            output_dir=output_dir,
+            num_frames_in_animation=self.num_frames_in_animation)
+
+        omnisuite_world_map_animator = OmniSuiteWorldMapAnimator(
+            grid, self._config)
+
         return omnisuite_world_map_animator
 
     def assert_animate(self):
@@ -21,10 +32,10 @@ class TestOmniSuiteWorldMapAnimator(AnimatorTestMixin, unittest.TestCase):
         An animation was successful when the number of frames in saved
         animation (e.g., gif) matches the expected amount of frames.
         """
-        raise NotImplementedError
-
-    def cleanup_animate(self):
-        pass
+        with Image.open(self._config.path_to_save_animation) as gif:
+            n_frames_in_gif = gif.n_frames
+            self.assertEqual(n_frames_in_gif, self.num_frames_in_animation)
+        return
 
 
 if __name__ == "__main__":
