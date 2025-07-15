5a8f2f1ec9e33ea70cf1a5975b1fec579710b3a1
Jared Frazier <frazier@iap-kborn.de>
2025-07-15 11:36:47 +0200

    unable to fix geopotential to height... moving on

--- tests/exploratory/test_xarr.py
+++ tests/exploratory/test_xarr.py
@@ -1,8 +1,14 @@
-from unittest import TestCase, main, skip
+from unittest import TestCase, main, skip, skipIf
 import netCDF4  # prevents xarray warning
 import xarray as xarr
-# from metpy.calc import geopotential_to_height
-# from metpy.units import units
+from os.path import exists
+try:
+    from metpy.calc import geopotential_to_height
+    from metpy.units import units
+    skip_geopotential_to_height_test = False
+except ModuleNotFoundError:
+    skip_geopotential_to_height_test = True
+
 from pdb import set_trace
 
 
@@ -15,8 +21,8 @@ class TestXarrayOps(TestCase):
             "/work/bm1233/m300685/UAICON/modes/inverse/GWS_202212010000061.nc"
         ]
         cls.gravity_wave_levante_files_glob = "/work/bm1233/m300685/UAICON/modes/inverse/GWS_202212*006*"
-        cls.gravity_wave_file = "data/gravity_waves/GWS_202603010000053.nc"
-
+        cls.local_gravity_wave_file = "data/gravity_waves/GWS_202603010000053.nc"
+        cls.levante_gravity_wave_file = "/work/bm1233/m300685/UAICON/modes/inverse/GWS_202212010000060.nc"
         return
 
     def test_open_mfdataset_on_files(self):
@@ -32,17 +38,21 @@ class TestXarrayOps(TestCase):
     def test_open_mfdataset_on_file(self):
         """open single file"""
         gravity_wave_single_mfdatset = xarr.open_mfdataset(
-            self.gravity_wave_file)
+            self.local_gravity_wave_file)
         shape = list(gravity_wave_single_mfdatset.sizes.values())
         time_dim = shape[0]
         expected_time_dim = 1
         self.assertEqual(time_dim, expected_time_dim)
         return
 
-    @skip("slow conversion process")
+    @skipIf(skip_geopotential_to_height_test, "metpy not found")
     def test_geopotential_height_to_geometric_height(self):
-        gravity_wave_single_mfdatset = xarr.open_mfdataset(
-            self.gravity_wave_file)
+        """Convert geopotential height to geometric height"""
+        if exists(self.local_gravity_wave_file):
+            gravity_wave_file = self.local_gravity_wave_file
+        elif exists(self.levante_gravity_wave_file):
+            gravity_wave_file = self.levante_gravity_wave_file
+        gravity_wave_single_mfdatset = xarr.open_mfdataset(gravity_wave_file)
         geopotential_height_var_name = "Z"
         geopotential_height = (
             gravity_wave_single_mfdatset
@@ -51,19 +61,22 @@ class TestXarrayOps(TestCase):
             .values
         )
 
+        # geopot_height = geopot/g0 --> geopot_height*g0 = geopot
+        # TODO: ... but this not yielding values in the expected range...
+        # known max altitude is ~ 120km 
         STANDARD_GRAVITY: float = 9.80665
-        geopotential = geopotential_height / STANDARD_GRAVITY
+        geopotential = geopotential_height * STANDARD_GRAVITY
         geopotential_quantities = units.Quantity(geopotential, "m^2/s^2")
 
-        # TODO: this does not seem to be the correct calculation because
         geometric_height = geopotential_to_height(geopotential_quantities)
+        set_trace()
 
         return
 
     def test_open_mfdataset_on_glob(self):
         """Open on glob Automatically concatenates along the time dimension!!"""
         gravity_wave_mfdataset = xarr.open_mfdataset(
-            self.gravity_wave_files_glob)
+            self.local_gravity_wave_files_glob)
         zonal_wind_var_name = "u"
         u_variable = gravity_wave_mfdataset.variables.get(zonal_wind_var_name)
         t = 0
