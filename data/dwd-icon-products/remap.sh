d66b9ab7cba7b32fb7545151344b18bb4495797b
Jared Frazier <cscidev001@gmail.com>
2025-07-29 16:38:52 +0200

    simple remap script for remapping ICON dwd grib data to full gaussian

---
+++ data/dwd-icon-products/remap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+# @brief CDO remap of ICON DWD Global to full Gaussian grid
+# @todo
+#   * support for other remap operations as argument
+#     see https://code.mpimet.mpg.de/projects/cdo/embedded/index.html#x1-280001.5
+dwd_icon_grib_file=$1
+usage="usage: $0 DWD_ICON_GRIB_FILE"
+if [[ -z "${dwd_icon_grib_file// }" ]]
+then
+    echo "$usage"
+    exit 1
+fi
+
+cdo gennn,F256 icon_grid_0026_R03B07_G.nc F256_weights.nc
+cdo -f nc remap,F256,F256_weights.nc $dwd_icon_grib_file ${dwd_icon_grib_file%.nc}_F256.nc
