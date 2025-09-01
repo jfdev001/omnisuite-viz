56d6a03bdfc4b12667d6a25618fa707deb98361a
Jared Frazier <72281389+jfdev001@users.noreply.github.com>
2025-09-01 13:34:32 +0100

    Jfrazier/72 gws figure (#73)
    * WIP: Long time span gravity wave file animation
    
    * Optimization load only desired response variable
    
    TODO:
    * Time loading is not occurring correctly, i.e., of 244 files that are
    in /work/bm1233/m300685/UAICON/modes/inverse/GWS_2022*, only 56 get
    loaded by xarr open
    * Note, for 56 files, dataset is 18 GB so keep this in mind... you may
    need to do the pre-emptive level select for efficiency... not sure if
    this even possibel though
    
    * reproducible multiyear gravity wave dataaset example
    
    ---------
    
    Co-authored-by: Jared Frazier <frazier@iap-kborn.de>

--- examples/plot_timelapsed_icon_multifiles_on_plate_carree_projection.py
+++ examples/plot_timelapsed_icon_multifiles_on_plate_carree_projection.py
@@ -1,27 +1,33 @@
 from __future__ import annotations
+from omnisuite_viz.reader import AbstractReader
+from omnisuite_viz.grid import WorldMapNetcdfGrid
+from omnisuite_viz.animator_config import NetcdfAnimatorConfig
+from omnisuite_viz.animator import OmniSuiteWorldMapAnimator
+
+from numpy.ma import MaskedArray
+from numpy import ndarray
+import xarray as xarr
+import netCDF4
+from matplotlib.pyplot import imread
 
 from argparse import (
     ArgumentParser, BooleanOptionalAction, RawTextHelpFormatter)
 from dataclasses import dataclass
-from matplotlib.pyplot import imread
-# from metpy.calc import geopotential_to_height
-import netCDF4
-import xarray as xarr
-from numpy import ndarray
-from numpy.ma import MaskedArray
 from os import environ
-from os.path import abspath, exists
+from os.path import abspath
 from pathlib import Path
-from typing import ClassVar, Dict
-from xarray.core.dataset import Variable
+from time import time
+from typing import ClassVar
 
-from omnisuite_viz.animator import OmniSuiteWorldMapAnimator
-from omnisuite_viz.animator_config import NetcdfAnimatorConfig
-from omnisuite_viz.grid import WorldMapNetcdfGrid
-from omnisuite_viz.reader import AbstractReader
+from cdo import Cdo
+print("Loading CDO...")
+cdo = Cdo()
+
+SECONDS_PER_MINUTE: int = 60
+
 
 DESCRIPTION = """
-Save animation frames (and optionally combine the frames to a gif) of
+Save animation frames (and optionally combine the frames to a gif) 
 by processing multifile ICON netcdf outputs (e.g., gravity waves, balances).
 
 Illustrates animating data that exists across many files (e.g., loaded in
@@ -47,7 +53,8 @@ def main():
     alpha: float = args.alpha
 
     # read args
-    netcdf_response_var_file_path: str = args.netcdf_response_var_file_path
+    netcdf_response_var_file_path: list[str] = (
+        args.netcdf_response_var_file_path)
 
     netcdf_response_var_short_name: str = (
         args.netcdf_response_var_short_name)
@@ -75,7 +82,18 @@ def main():
         max_vertical_layer_height_in_meters=(
             max_vertical_layer_height_in_meters))
 
+    print("Reading data...")
+    start_read = time()
     reader.read()
+    end_read = time()
+    elapsed_read = end_read - start_read
+    read_time_in_minutes = int(elapsed_read // SECONDS_PER_MINUTE)
+    read_time_in_seconds = elapsed_read % SECONDS_PER_MINUTE
+    print(
+        "Time spent reading data:"
+        f" {read_time_in_minutes}m {read_time_in_seconds:.2f}s")
+
+    print("Postprocessing data...")
     reader.postprocess()
 
     grid = reader.grid
@@ -96,7 +114,6 @@ def main():
         netcdf_var_cmap_on_plot=cmap,
         netcdf_var_transparency_on_plot=alpha,
 
-
         netcdf_response_var_file_path=netcdf_response_var_file_path,
         blue_marble_path=blue_marble_path)
 
@@ -104,6 +121,7 @@ def main():
     animator = ICONModelAnimator(
         grid=grid, config=config, blue_marble_img=blue_marble_img)
 
+    print("Making animation...")
     animator.animate()
 
     return
@@ -143,19 +161,17 @@ def cli():
     parser.add_argument(
         "--save-animation",
         action=BooleanOptionalAction,
+        help="Flag to save animation as gif from generated frames. (required).",
         required=True)
 
     read_group = parser.add_argument_group("read")
 
-    default_netcdf_response_var_file_path = abspath(
-        "data/gravity_waves/*.nc")
     read_group.add_argument(
         "--netcdf-response-var-file-path",
-        default=default_netcdf_response_var_file_path,
         nargs="+",
         type=str,
-        help="path or paths or file glob to netcdf input data."
-        f" (default: {default_netcdf_response_var_file_path})")
+        help="path or paths or file glob to netcdf input data. (required)",
+        required=True)
 
     default_netcdf_response_var_short_name: str = "u"
     read_group.add_argument(
@@ -277,16 +293,37 @@ class ICONMultifileDataReader(AbstractReader):
         self.max_vertical_layer_height_in_meters = (
             max_vertical_layer_height_in_meters)
 
+        # Initialize read variables
+        self.mfdataset = None
+        self.response = None
+        self.latitude = None
+        self.longitude = None
+
         # TODO: make property?
         self.blue_marble_img = None
         return
 
     def read(self):
+        # load only the response variable to save memory
+        # TODO: optimization consideration... can you also only load
+        # the desired levels?? e.g., if you only need level 72, just load that..
+        data_vars = cdo.showname(
+            input=self.netcdf_response_var_file_path[0],
+            autoSplit=' ')
+        data_vars_to_drop = [
+            var for var in data_vars
+            if var != self.netcdf_response_var_short_name]
+
+        # time consuming and memory intensive read
         self.mfdataset = xarr.open_mfdataset(
-            self.netcdf_response_var_file_path)
+            self.netcdf_response_var_file_path,
+            drop_variables=data_vars_to_drop)
 
         # TODO: this is not memory efficient because it gathers the
-        # chunked data into a single numpy array
+        # chunked data into a single numpy array... though it doesn't seem
+        # like this operation takes a long time... the memory intensive
+        # portion seems to be in the xarr open... maybe these are just pointers
+        # to the opened dataset...
         self.response = (
             self.mfdataset
             .variables
@@ -305,12 +342,6 @@ class ICONMultifileDataReader(AbstractReader):
             .get(ICONConfigConsts.LONGITUDE_NETCDF_SHORT_VAR_NAME)
             .values)
 
-        self.geopotential_height = (
-            self.mfdataset
-            .variables
-            .get(ICONConfigConsts.GEOPOTENTIAL_HEIGHT_NETCDF_SHORT_VAR_NAME)
-            .values)
-
         # for plotting blue marble in animator (note: move elsewhere??)
         self.blue_marble_img = imread(self.blue_marble_path)
 
@@ -321,10 +352,9 @@ class ICONMultifileDataReader(AbstractReader):
             self.response = self.response[:, self.level_ix, :, :]
         else:
             raise NotImplementedError(
-                "Uncertain how to get geometric height from fields in netdcf."
-                " Discussion with P. Ghosh on earlier problem indicates that"
-                " that /work/bm1233/b383395/icon-visualization-examples/scripts/modes_GWsDensity_032019_climatology_test.py"
-                " contains the generalized height and sigma map 1:1")
+                " sigma = pressure/surface_pressure = P/P0, so you can get"
+                " geometric height z using scale height z = H*ln(P0/P)"
+                " https://github.com/jfdev001/omnisuite-viz/issues/33#issuecomment-3052705623")
             # TODO: move to utils func
             # TODO: you need to use geopotential to height here
             # Use the upper and lower bounds of height to average response var...
